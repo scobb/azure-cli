4d421b7f0d2aaddf8d7221bf4c7b56f87896267b
Steve Cobb (AZURE MACHINE LEARNING) <stcob@microsoft.com>
2017-03-21 10:44:33 -0700

    Renaming aml module to ml.

---
+++ azure/cli/command_modules/ml/_help.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core.help_files import helps
+
+# pylint: disable=line-too-long
+
+helps['ml'] = """
+            type: group
+            short-summary: Module for operationalizing machine learning models
+            """
