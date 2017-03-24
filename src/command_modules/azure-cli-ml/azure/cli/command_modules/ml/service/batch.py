51f7df92f588520651c21d9906dd9a54a62da880
Steve Cobb (AZURE MACHINE LEARNING) <stcob@microsoft.com>
2017-03-24 14:37:33 -0700

    Run is up and going.

--- src/command_modules/azure-cli-ml/azure/cli/command_modules/ml/service/batch.py
+++ src/command_modules/azure-cli-ml/azure/cli/command_modules/ml/service/batch.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from ._util import TableResponse
 from ._util import cli_context
 from ._batchutilities import batch_env_is_valid
@@ -33,6 +34,8 @@ from ._batchutilities import batch_create_parameter_list
 import json
 from ._util import StaticStringWithTableReponse
 from ._batchutilities import batch_create_service_header_to_fn_dict
+from ._batchutilities import BATCH_SINGLE_JOB_FMT
+from ._batchutilities import validate_and_split_run_param
 
 
 def batch_service_list(context=cli_context):
@@ -49,7 +52,8 @@ def batch_service_list(context=cli_context):
         print(get_success_and_resp_str(context, resp, response_obj=TableResponse(
             batch_list_service_header_to_fn_dict))[1])
     except requests.ConnectionError:
-        print("Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
+        print(
+        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
         return
 
 
@@ -67,20 +71,26 @@ def batch_service_view(service_name, verb, context=cli_context):
     try:
         resp = context.http_call('get', url, auth=(context.hdi_user, context.hdi_pw))
 
-        success, response = get_success_and_resp_str(context, resp, response_obj=MultiTableResponse(
-            [batch_view_service_header_to_fn_dict, batch_view_service_usage_header_to_fn_dict]), verbose=verb)
+        success, response = get_success_and_resp_str(context, resp,
+                                                     response_obj=MultiTableResponse(
+                                                         [
+                                                             batch_view_service_header_to_fn_dict,
+                                                             batch_view_service_usage_header_to_fn_dict]),
+                                                     verbose=verb)
         print(response)
         if success:
             param_str = ' '.join([batch_get_parameter_str(p) for
                                   p in sorted(resp.json()['Parameters'],
                                               key=lambda x: '_' if 'Value' in x
                                               else x['Direction'])])
-            usage = 'Usage: aml service run batch -n {} {} [-w] [-j <job_id>] [-v]'.format(service_name,
-                                                                                           param_str)
+            usage = 'Usage: az ml service run batch -n {} {} [-w] [-j <job_id>] [-v]'.format(
+                service_name,
+                param_str)
             print(usage)
 
     except requests.ConnectionError:
-        print("Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
+        print(
+        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
         return
 
 
@@ -95,7 +105,9 @@ def batch_view_job(service_name, job_name, verb, context=cli_context):
     if not batch_env_is_valid(context):
         return
 
-    success, content = get_success_and_resp_str(context, batch_get_job(context, job_name, service_name, verb),
+    success, content = get_success_and_resp_str(context, batch_get_job(context, job_name,
+                                                                       service_name,
+                                                                       verb),
                                                 verbose=verb)
     if success:
         print(batch_get_job_description(context, content))
@@ -116,9 +128,11 @@ def batch_list_jobs(service_name, context=cli_context):
     url = batch_get_url(context, BATCH_ALL_JOBS_FMT, service_name)
     try:
         resp = context.http_call('get', url, auth=(context.hdi_user, context.hdi_pw))
-        print(get_success_and_resp_str(context, resp, response_obj=TableResponse(batch_list_jobs_header_to_fn_dict))[1])
+        print(get_success_and_resp_str(context, resp, response_obj=TableResponse(
+            batch_list_jobs_header_to_fn_dict))[1])
     except requests.ConnectionError:
-        print("Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
+        print(
+        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
 
 
 def batch_cancel_job(service_name, job_name, verb, context=cli_context):
@@ -138,13 +152,13 @@ def batch_cancel_job(service_name, job_name, verb, context=cli_context):
         resp = context.http_call('post', url,
                                  auth=(context.hdi_user, context.hdi_pw))
         print(
-        get_success_and_resp_str(context, resp, response_obj=StaticStringResponse(
-            'Job {0} of service {1} canceled.'.format(job_name, service_name)),
-                                 verbose=verb)[1])
+            get_success_and_resp_str(context, resp, response_obj=StaticStringResponse(
+                'Job {0} of service {1} canceled.'.format(job_name, service_name)),
+                                     verbose=verb)[1])
     except requests.ConnectionError:
         print(
-        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(
-            url))
+            "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(
+                url))
 
 
 def batch_service_delete(service_name, verb, context=cli_context):
@@ -162,7 +176,8 @@ def batch_service_delete(service_name, verb, context=cli_context):
     try:
         resp = context.http_call('get', url, auth=(context.hdi_user, context.hdi_pw))
     except requests.ConnectionError:
-        print("Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
+        print(
+        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
         return
 
     exists, err_msg = get_success_and_resp_str(context, resp, verbose=verb)
@@ -175,10 +190,111 @@ def batch_service_delete(service_name, verb, context=cli_context):
     try:
         resp = context.http_call('delete', url, auth=(context.hdi_user, context.hdi_pw))
     except requests.ConnectionError:
-        print("Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
+        print(
+        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
         return
     print(get_success_and_resp_str(context, resp, response_obj=StaticStringResponse(
         'Service {} deleted.'.format(service_name)), verbose=verb)[1])
+
+
+def batch_service_run(service_name, verb, inputs,
+                      outputs, parameters, job_name,
+                      wait_for_completion,
+                      context=cli_context):
+    if not job_name:
+        job_name = time.strftime('%Y-%m-%d_%H%M%S')
+
+    parameter_dict = {}
+    valid_parameters = True
+    for param in inputs:
+        is_valid, k, v = validate_and_split_run_param(param)
+        valid_parameters = valid_parameters and is_valid
+        if valid_parameters:
+            parameter_dict[k] = v, True
+
+    for param in outputs + parameters:
+        is_valid, k, v = validate_and_split_run_param(param)
+        valid_parameters = valid_parameters and is_valid
+        if valid_parameters:
+            parameter_dict[k] = v, False
+
+    if not valid_parameters:
+        return
+
+    parameters_container = 'parameters{}'.format(uuid.uuid4())
+
+    # validate environment
+    if not batch_env_and_storage_are_valid(context):
+        return
+
+    # upload any local parameters if needed
+    try:
+        parameter_dict = {param_name: update_asset_path(context, verb,
+                                                        parameter_dict[param_name][0],
+                                                        parameters_container,
+                                                        is_input=
+                                                        parameter_dict[param_name][
+                                                            1] == '-i')[1]
+                          for param_name in parameter_dict}
+    except ValueError as exc:
+        print('Unable to process parameters: {}'.format(exc))
+        return
+
+    if verb:
+        print('parameters: {0}'.format(parameter_dict))
+
+    arg_payload = {'Parameters': parameter_dict}
+
+    url = batch_get_url(context, BATCH_SINGLE_JOB_FMT, service_name, job_name)
+    if verb:
+        print('scoring at {}'.format(url))
+    headers = {'Content-Type': 'application/json'}
+    try:
+        resp = context.http_call('put', url,
+                                 headers=headers,
+                                 data=json.dumps(arg_payload),
+                                 auth=(context.hdi_user, context.hdi_pw))
+    except requests.ConnectionError:
+        print(
+            "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(
+                url))
+        return
+
+    succeeded, to_print = get_success_and_resp_str(context, resp,
+                                                   response_obj=StaticStringResponse(
+                                                       'Job {0} submitted on service {1}.'.format(
+                                                           job_name, service_name)),
+                                                   verbose=verb)
+    print(to_print)
+    if not succeeded:
+        return
+
+    if wait_for_completion:
+        while True:
+            succeeded, to_print = get_success_and_resp_str(context,
+                                                           batch_get_job(context,
+                                                                         job_name,
+                                                                         service_name,
+                                                                         verb),
+                                                           verbose=verb)
+            if not succeeded:
+                print(to_print)
+                return
+            resp_obj = get_json(to_print)
+            job_state = resp_obj['State']
+            if 'YarnAppId' in resp_obj:
+                print('{}: {}'.format(resp_obj['YarnAppId'], job_state))
+            else:
+                print(job_state)
+            if job_state == 'NotStarted' or job_state == 'InProgress':
+                time.sleep(5)
+            else:
+                print(batch_get_job_description(context, to_print))
+                break
+    else:
+        print(
+            'To check job status, run: az ml service viewjob batch -n {} -j {}'.format(
+                service_name, job_name))
 
 
 def batch_service_create(driver_file, service_name, title, verb, inputs,
@@ -210,14 +326,16 @@ def batch_service_create(driver_file, service_name, title, verb, inputs,
     # DEPENDENCIES
     dependency_container = 'dependencies/{}'.format(uuid.uuid4())
     try:
-        dependencies = [update_asset_path(context, verb, dependency, dependency_container) for dependency in dependencies]
+        dependencies = [update_asset_path(context, verb, dependency, dependency_container)
+                        for dependency in dependencies]
     except ValueError as exc:
         print('Error uploading dependencies: {}'.format(exc))
         return
 
     # DRIVER
     try:
-        driver_id, driver_uri = update_asset_path(context, verb, driver_file, dependency_container)
+        driver_id, driver_uri = update_asset_path(context, verb, driver_file,
+                                                  dependency_container)
     except ValueError as exc:
         print('Error uploading driver: {}'.format(exc))
         return
@@ -236,14 +354,16 @@ def batch_service_create(driver_file, service_name, title, verb, inputs,
             dependency[0])
 
     # replace inputs from template
-    json_payload['Parameters'] = batch_create_parameter_list(inputs + outputs + parameters)
+    json_payload['Parameters'] = batch_create_parameter_list(
+        inputs + outputs + parameters)
 
     # update assets payload for default inputs
     for parameter in json_payload['Parameters']:
         if 'Value' in parameter:
             if parameter['Kind'] == 'Reference':
                 try:
-                    asset_id, location = update_asset_path(context, verb, parameter['Value'],
+                    asset_id, location = update_asset_path(context, verb,
+                                                           parameter['Value'],
                                                            dependency_container,
                                                            parameter['Direction'] ==
                                                            'Input')
@@ -269,10 +389,11 @@ def batch_service_create(driver_file, service_name, title, verb, inputs,
 
     try:
         resp = context.http_call('put', url, headers=headers,
-                            data=json.dumps(json_payload),
-                            auth=(context.hdi_user, context.hdi_pw))
+                                 data=json.dumps(json_payload),
+                                 auth=(context.hdi_user, context.hdi_pw))
     except requests.ConnectionError:
-        print("Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
+        print(
+        "Error connecting to {}. Please confirm SparkBatch app is healthy.".format(url))
         return
 
     # Create usage str: inputs/parameters before ouputs, optional after all
@@ -281,11 +402,15 @@ def batch_service_create(driver_file, service_name, title, verb, inputs,
                                       key=lambda x: '_' if 'Value' in x
                                       else x['Direction'])])
 
-    usage = 'Usage: aml service run batch -n {} {} [-w] [-j <job_id>] [-v]'.format(service_name,
-                                                                                   param_str)
+    usage = 'Usage: az ml service run batch -n {} {} [-w] [-j <job_id>] [-v]'.format(
+        service_name,
+        param_str)
 
-    success, response = get_success_and_resp_str(context, resp, response_obj=StaticStringWithTableReponse(
-        usage, batch_create_service_header_to_fn_dict), verbose=verb)
+    success, response = get_success_and_resp_str(context, resp,
+                                                 response_obj=StaticStringWithTableReponse(
+                                                     usage,
+                                                     batch_create_service_header_to_fn_dict),
+                                                 verbose=verb)
     if success:
         print('Success.')
 
