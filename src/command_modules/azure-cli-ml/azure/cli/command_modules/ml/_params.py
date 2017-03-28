fa83c0a6286f08d508981b32ebc83c08f71c722a
Ritwik Bhattacharya <ritwik.bhattacharya@gmail.com>
2017-03-28 07:02:04 -0700

    Adding delete and run support.

--- src/command_modules/azure-cli-ml/azure/cli/command_modules/ml/_params.py
+++ src/command_modules/azure-cli-ml/azure/cli/command_modules/ml/_params.py
@@ -53,6 +53,8 @@ register_cli_argument('ml service create realtime', 'target_runtime', options_li
                       help='Runtime of the web service. Valid runtimes are {}'.format('|'.join(RealtimeConstants.supported_runtimes)), required=False)
 register_cli_argument('ml service create realtime', 'logging_level', options_list='-l', default='none', const = 'debug',
                       nargs='?', help='Logging level. Valid levels are {}'.format('|'.join(RealtimeConstants.supported_logging_levels)), required=False)
+register_cli_argument('ml service run realtime', 'input_data', options_list='-d', default='',
+                      help='The data to use for calling the web service.', required=False)
 
 # env workflows
 register_cli_argument('ml env cluster', 'force_connection', options_list='-f', action='store_true',
