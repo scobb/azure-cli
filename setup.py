5ba4f006c01da5162aa70f6b9c2884a68ab367fb
Steve Cobb (AZURE MACHINE LEARNING) <stcob@microsoft.com>
2017-04-17 16:57:54 -0700

    Check through ContainerServiceClient if k8s cluster already exists.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-cli-core',
-    'azure-mgmt-compute==0.33.1rc1',
+    'azure-mgmt-compute==1.0.0rc1',
     'azure-mgmt-containerregistry==0.2.0',
     'azure-mgmt-resource==0.30.2',
     'azure-mgmt-storage==0.31.0',
