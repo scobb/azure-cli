81f9493342530a8f452dffaf554ce50506140adc
Steve Cobb (AZURE MACHINE LEARNING) <stcob@microsoft.com>
2017-04-26 11:13:28 -0700

    Messing around with paramiko port forwarding.

--- src/command_modules/azure-cli-ml/tests/e2e_tests/realtime_e2e_tests.py
+++ src/command_modules/azure-cli-ml/tests/e2e_tests/realtime_e2e_tests.py
@@ -5,6 +5,14 @@ from azure.cli.command_modules.ml.service.realtime import realtime_service_list
 
 
 class RealtimeE2eTests(unittest.TestCase):
+    local_context = E2eContext()
+    local_context.local_mode = True
+    local_context.az_account_name = 'stcobmesosstor'
+    local_context.az_account_key = 'qqlajVsnlLW1GZ0zSTYR5T/uONvILUoogQPQBQREQHbW61cDwr4gycNNjYLQ6MuXQAEJq5Q05v2PKGBXzLqTsQ=='
+    local_context.acr_home = 'stcobmesosacr.azurecr.io'
+    local_context.acr_user = 'stcobmesosacr'
+    local_context.acr_pw = '+=wNR+h/+BG++/=mi6=7T5C6=HFE/Q/P'
+
     kube_context = E2eContext()
     kube_context.env_is_k8s = True
     kube_context.az_account_name = 'stcobwink8s2stor'
@@ -15,16 +23,69 @@ class RealtimeE2eTests(unittest.TestCase):
     kube_context.app_insights_account_name = 'scwink8sacrapp_ins'
     kube_context.app_insights_account_key = '913c6712-3cf5-4a71-8f28-6019b51b6af1'
 
+    mesos_context = E2eContext()
+    mesos_context.env_is_k8s = False
+    mesos_context.az_account_name = 'stcobmesosstor'
+    mesos_context.az_account_key = 'qqlajVsnlLW1GZ0zSTYR5T/uONvILUoogQPQBQREQHbW61cDwr4gycNNjYLQ6MuXQAEJq5Q05v2PKGBXzLqTsQ=='
+    mesos_context.acr_home = 'stcobmesosacr.azurecr.io'
+    mesos_context.acr_user = 'stcobmesosacr'
+    mesos_context.acr_pw = '+=wNR+h/+BG++/=mi6=7T5C6=HFE/Q/P'
+    mesos_context.app_insights_account_name = 'stcobmesosapp_ins'
+    mesos_context.app_insights_account_key = 'dce4b677-19bd-4dc6-9edd-bdbb87804c33'
+    mesos_context.acs_master_url = 'stcobmesosacsmaster.eastus.cloudapp.azure.com'
+    mesos_context.acs_agent_url = 'stcobmesosacsagent.eastus.cloudapp.azure.com'
+
+    @staticmethod
+    def setup_tunnel():
+        from forward import forward_tunnel
+        import socket
+        import paramiko
+        import os
+
+        # Command for paramiko-1.7.7.1
+        # Find a random unbound port
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('', 0))
+        local_port = sock.getsockname()[1]
+
+        client = paramiko.SSHClient()
+        acs_id_rsa = os.path.join(os.path.expanduser('~'), '.ssh', 'acs_id_rsa_stcob_lnx2')
+        client.load_system_host_keys(acs_id_rsa)
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
+        try:
+
+            client.connect(RealtimeE2eTests.mesos_context.acs_master_url,
+                           port=2200,
+                           username='acsadmin',
+                           key_filename=acs_id_rsa)
+
+        except Exception as e:
+
+            print('*** Failed to connect to %s:%d: %r' % (RealtimeE2eTests.mesos_context.acs_master_url, 2200, e))
+            return
+
+        reverse_forward_tun
+        transport = paramiko.Transport(sock)
+        transport.connect(username='acsadmin')
+
+        print('Forwarding local port {} to port 80 on your ACS cluster'.format(local_port))
+        forward_tunnel(local_port, RealtimeE2eTests.mesos_context.acs_master_url, 2200, transport)
+
 
     def test_list_local(self):
         c = E2eContext()
         c.local_mode = True
         realtime_service_list(context=c)
+        if not hasattr(sys.stdout, "getvalue"):
+            self.fail("need to run in buffered mode")
+        output = sys.stdout.getvalue().strip()
+        self.assertTrue(output.startswith('+------'))
+        self.assertTrue(output.endswith('-----+'))
 
     def test_list_mesos(self):
-        c = E2eContext()
-        c.local_mode = False
-        realtime_service_list(context=c)
+        self.setup_tunnel()
+        realtime_service_list(context=self.mesos_context)
+
         if not hasattr(sys.stdout, "getvalue"):
             self.fail("need to run in buffered mode")
         output = sys.stdout.getvalue().strip()
@@ -38,7 +99,63 @@ class RealtimeE2eTests(unittest.TestCase):
         self.assertTrue(output.startswith('+------'))
         self.assertTrue(output.endswith('-----+'))
 
+def reverse_forward_tunnel(server_port, remote_host, remote_port, transport):
+    import threading
+    transport.request_port_forward('', server_port)
 
+    while True:
+
+        chan = transport.accept(1000)
+
+        if chan is None:
+            continue
+
+        thr = threading.Thread(target=handler, args=(chan, remote_host, remote_port))
+        thr.setDaemon(True)
+        thr.start()
+
+def handler(chan, host, port):
+    import socket
+    sock = socket.socket()
+
+    try:
+        sock.connect((host, port))
+
+    except Exception as e:
+        print('Forwarding request to %s:%d failed: %r' % (host, port, e))
+
+    print ('Connected! Tunnel open %r -&gt; %r -&gt; %r' % (chan.origin_addr,
+                   chan.getpeername(), (host, port)))
+
+    while True:
+
+    r, w, x = select.select([sock, chan], [], [])
+
+    if sock in r:
+
+    data = sock.recv(1024)
+
+    if len(data) == 0:
+
+    break
+
+    chan.send(data)
+
+    if chan in r:
+
+    data = chan.recv(1024)
+
+    if len(data) == 0:
+
+    break
+
+    sock.send(data)
+
+    chan.close()
+
+    sock.close()
+
+    verbose('Tunnel closed from %r' % (chan.origin_addr,))
 if __name__ == '__main__':
     assert not hasattr(sys.stdout, "getvalue")
     unittest.main(module=__name__, buffer=True, exit=False)
