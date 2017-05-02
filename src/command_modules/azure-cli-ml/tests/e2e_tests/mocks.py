5570ce22defd38d906cc185d84b86f96d2b3c819
Steve Cobb (AZURE MACHINE LEARNING) <stcob@microsoft.com>
2017-05-02 16:59:57 -0700

    Figured out the threading issue. Forwarding should now be able to work on windows, though I need to test.

--- src/command_modules/azure-cli-ml/tests/e2e_tests/mocks.py
+++ src/command_modules/azure-cli-ml/tests/e2e_tests/mocks.py
@@ -1,10 +1,11 @@
+import os
 from azure.cli.command_modules.ml._util import CommandLineInterfaceContext
 
 
 class E2eContext(CommandLineInterfaceContext):
     def __init__(self):
         super(E2eContext, self).__init__()
-        self.local_mode = False
+        self.local_mode = bool(os.environ.get('AML_LOCAL_MODE', 'False'))
         self.forwarded_port = -1
 
     def in_local_mode(self):
