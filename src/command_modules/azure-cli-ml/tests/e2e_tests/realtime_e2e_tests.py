5e9ec8f23c3a43ce3bb8b03f78479525b954ada2
Steve Cobb (AZURE MACHINE LEARNING) <stcob@microsoft.com>
2017-05-05 13:03:26 -0700

    Temporarily commenting local tests. View, List, and ViewNonExistent up and going.

--- src/command_modules/azure-cli-ml/tests/e2e_tests/realtime_e2e_tests.py
+++ src/command_modules/azure-cli-ml/tests/e2e_tests/realtime_e2e_tests.py
@@ -1,8 +1,8 @@
 import unittest
 import sys
-import threading
 from mocks import E2eContext
 from azure.cli.command_modules.ml.service.realtime import realtime_service_list
+from azure.cli.command_modules.ml.service.realtime import realtime_service_view
 
 
 class UserScenario(object):
@@ -26,6 +26,34 @@ class ListScenario(UserScenario):
         output = sys.stdout.getvalue().strip()
         self.verify.assertTrue(output.startswith('+------'))
         self.verify.assertTrue(output.endswith('-----+'))
+
+
+class ViewScenario(UserScenario):
+    def test_scenario(self):
+        realtime_service_view('basic', context=self.context)
+        if not hasattr(sys.stdout, "getvalue"):
+            self.verify.fail("need to run in buffered mode")
+        output = sys.stdout.getvalue().strip()
+        lines = output.split('\n')
+        self.verify.assertTrue(lines[0].startswith('+--------+'))
+        self.verify.assertTrue(lines[1].startswith('| NAME   |'))
+        self.verify.assertTrue(lines[2].startswith('|--------+'))
+        print(lines[3])
+        print('| basic  | {}/basic'.format(self.context.acr_home))
+        self.verify.assertTrue(lines[3].startswith('| basic  | {}/basic'.format(self.context.acr_home)))
+        self.verify.assertTrue(lines[4].startswith('+--------+'))
+        self.verify.assertEqual(lines[5], 'Usage:')
+        self.verify.assertTrue(lines[6].startswith('  az ml  : az ml service run realtime -n basic '))
+        self.verify.assertTrue(lines[7].startswith('  curl : curl -X POST -H "Content-Type:application/json"'))
+
+
+class ViewNonExistentScenario(UserScenario):
+    def test_scenario(self):
+        realtime_service_view('nonexistent_service', context=self.context)
+        if not hasattr(sys.stdout, "getvalue"):
+            self.verify.fail("need to run in buffered mode")
+        output = sys.stdout.getvalue().strip()
+        self.verify.assertEqual(output, 'No service running with name nonexistent_service on your ACS cluster')
 
 
 class TestManager(unittest.TestCase):
@@ -74,80 +102,17 @@ mesos_context.acs_master_url = 'stcobmesosacsmaster.eastus.cloudapp.azure.com'
 mesos_context.acs_agent_url = 'stcobmesosacsagent.eastus.cloudapp.azure.com'
 
 contexts = [
-    local_context,
+    # local_context,
     kube_context,
     mesos_context
 ]
 
 scenarios = [
     ListScenario,
+    ViewScenario,
+    ViewNonExistentScenario
 ]
-#
-# class RealtimeE2eTests(unittest.TestCase):
-#     local_context = E2eContext('local')
-#     local_context.local_mode = True
-#     local_context.az_account_name = 'stcobmesosstor'
-#     local_context.az_account_key = 'qqlajVsnlLW1GZ0zSTYR5T/uONvILUoogQPQBQREQHbW61cDwr4gycNNjYLQ6MuXQAEJq5Q05v2PKGBXzLqTsQ=='
-#     local_context.acr_home = 'stcobmesosacr.azurecr.io'
-#     local_context.acr_user = 'stcobmesosacr'
-#     local_context.acr_pw = '+=wNR+h/+BG++/=mi6=7T5C6=HFE/Q/P'
-#
-#     kube_context = E2eContext('k8s')
-#     kube_context.env_is_k8s = True
-#     kube_context.az_account_name = 'stcobwink8s2stor'
-#     kube_context.az_account_key = 'w53gYgs9Ydd6SPoMqLjQ60Ou29dhBts5UMUVIQuPca8q8+6vxgF2kVRhqFTXIP+ejcYRid6M6FyeGMlQRXnKmg=='
-#     kube_context.acr_home = 'stcobwink8s2acr.azurecr.io'
-#     kube_context.acr_user = 'stcobwink8s2acr'
-#     kube_context.acr_pw = '+=a=x=+=/i=Ix=mQL1Ar=mcDzsO/=ml/'
-#     kube_context.app_insights_account_name = 'stcobwink8s2app_ins'
-#     kube_context.app_insights_account_key = 'e7f8cf3c-7ce0-428a-96d5-aeed56b92ad8'
-#
-#     mesos_context = E2eContext('mesos')
-#     mesos_context.env_is_k8s = False
-#     mesos_context.az_account_name = 'stcobmesosstor'
-#     mesos_context.az_account_key = 'qqlajVsnlLW1GZ0zSTYR5T/uONvILUoogQPQBQREQHbW61cDwr4gycNNjYLQ6MuXQAEJq5Q05v2PKGBXzLqTsQ=='
-#     mesos_context.acr_home = 'stcobmesosacr.azurecr.io'
-#     mesos_context.acr_user = 'stcobmesosacr'
-#     mesos_context.acr_pw = '+=wNR+h/+BG++/=mi6=7T5C6=HFE/Q/P'
-#     mesos_context.app_insights_account_name = 'stcobmesosapp_ins'
-#     mesos_context.app_insights_account_key = 'dce4b677-19bd-4dc6-9edd-bdbb87804c33'
-#     mesos_context.acs_master_url = 'stcobmesosacsmaster.eastus.cloudapp.azure.com'
-#     mesos_context.acs_agent_url = 'stcobmesosacsagent.eastus.cloudapp.azure.com'
-#
-#     contexts = [
-#         local_context,
-#         kube_context,
-#         mesos_context
-#     ]
-#
-#     scenarios = [
-#         ListScenario,
-#     ]
-#
-#     def test_list_local(self):
-#         realtime_service_list(context=self.local_context)
-#         if not hasattr(sys.stdout, "getvalue"):
-#             self.fail("need to run in buffered mode")
-#         output = sys.stdout.getvalue().strip()
-#         self.assertTrue(output.startswith('+------'))
-#         self.assertTrue(output.endswith('-----+'))
-#
-#     def test_list_mesos(self):
-#         realtime_service_list(context=self.mesos_context)
-#
-#         if not hasattr(sys.stdout, "getvalue"):
-#             self.fail("need to run in buffered mode")
-#         output = sys.stdout.getvalue().strip()
-#         self.assertTrue(output.startswith('+------'))
-#         self.assertTrue(output.endswith('-----+'))
-#
-#     def test_list_kubernetes(self):
-#         realtime_service_list(context=self.kube_context)
-#         if not hasattr(sys.stdout, "getvalue"):
-#             self.fail("need to run in buffered mode")
-#         output = sys.stdout.getvalue().strip()
-#         self.assertTrue(output.startswith('+------'))
-#         self.assertTrue(output.endswith('-----+'))
+
 
 
 if __name__ == '__main__':
